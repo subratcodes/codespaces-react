27aa53f644193d8f9b169122c1a64e554d3ef2bd
Subrat Kumar Singh <85783015+subratcodes@users.noreply.github.com>
2024-06-12 04:01:38 +0000

    pagination logic remains

--- src/Components/PageComponent.jsx
+++ src/Components/PageComponent.jsx
@@ -6,25 +6,45 @@ function PageComponent({activePage,totalPages}){
 
   return <Container>
     <Pagination linkClassName="">
-    <Pagination.First />
+    
     <Pagination.Prev />
     <Pagination.Item>{1}</Pagination.Item>
-    <Pagination.Ellipsis />
+    
 
+    <Pagination.Item>{2}</Pagination.Item>
+    <Pagination.Item>{3}</Pagination.Item>
+    <Pagination.Item active>{4}</Pagination.Item>
+    <Pagination.Item>{5}</Pagination.Item>
+    <Pagination.Item disabled>{6}</Pagination.Item>
+
+    <Pagination.Ellipsis />
     <Pagination.Item>{10}</Pagination.Item>
-    <Pagination.Item>{11}</Pagination.Item>
-    <Pagination.Item active>{12}</Pagination.Item>
-    <Pagination.Item>{13}</Pagination.Item>
-    <Pagination.Item disabled>{14}</Pagination.Item>
-
-    <Pagination.Ellipsis />
-    <Pagination.Item>{20}</Pagination.Item>
     <Pagination.Next />
-    <Pagination.Last />
+   
   </Pagination>
 
   </Container>
 }
 
+/*
+
+ Items needed for the Pagianation.
+
+ {
+
+ currentPage=int.
+ listOfPages:[],
+ elipsisBreakPoint=true
+ elipsisSucceedingNo=10;
+ OnClickPrev=
+ onClickFirst=
+ onClickNext=
+ onClickLast=
+
+ }
+
+
+
+*/
 
 export default PageComponent;
